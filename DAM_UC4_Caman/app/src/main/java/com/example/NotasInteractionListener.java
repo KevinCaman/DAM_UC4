93028d5783cdfbbf4d4c91501af45fce98d8b52a
Kevin Caman <kevingabriel092001@gmail.com>
2023-07-10 15:12:27 -0500

    Notas en Columnas

---
+++ DAM_UC4_Caman/app/src/main/java/com/example/NotasInteractionListener.java
@@ -0,0 +1,9 @@
+package com.example;
+
+import com.example.Nota;
+
+public interface NotasInteractionListener {
+    void editNotaClick(Nota nota);
+    void eliminaNotaClick (Nota nota);
+    void favoritaNotaClick (Nota nota);
+}
--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.dam_uc4_caman;
+package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
